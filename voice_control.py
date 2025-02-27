5fc03e584750bdeba3ae7409397c30358b927deb
ryo08271154 <ryo20080827@gmail.com>
2025-02-27 22:13:38 +0900

    決めた単語が含まれていたら操作するように変更

--- voice_control.py
+++ voice_control.py
@@ -17,6 +17,11 @@ custom_scenes=json.load(open(os.path.join(dir_name,"custom_scenes.json")))
 custom_devices=json.load(open(os.path.join(dir_name,"custom_devices.json")))
 last_text=""
 reply=""
+words=["電気","天気"]
+devices_name=[i["deviceName"] for i in devices["body"]["infraredRemoteList"]]
+custom_devices_name=[i["deviceName"] for i in custom_devices["deviceList"]]
+words.extend(devices_name)
+words.extend(custom_devices_name)
 def always_on_voice():
     global last_text
     model = vosk.Model(os.path.join(dir_name,"model_2"))
@@ -44,33 +49,34 @@ def judge(text):
     action=None
     client=wit.Wit(os.getenv("WIT_TOKEN"))
     r=client.message(text)
-    if r['intents'] and r['entities']:
-        if r['intents'][0]['name']=='turnOn' or 'turnOff':
-            action=r['intents'][0]["name"]
-        if r['intents'][0]['name']=='weather' and r["entities"]:
-            action=datetime.datetime.fromisoformat(r["entities"]["wit$datetime:datetime"][0]["value"])
-    return action                
+    if r['intents']:
+        if r['entities']:
+            if r['intents'][0]['name'] in ['turnOn','turnOff']:
+                action=r['intents'][0]["name"]
+                device_name=[i["body"] for i in r["entities"]["device_name:device_name"]]
+                custom_device_control(device_name,action)
+                switchbot_device_control(device_name,action)
+        if r['intents'][0]['name']=='weather':
+            if r["entities"]:
+                action=datetime.datetime.fromisoformat(r["entities"]["wit$datetime:datetime"][0]["value"])
+            weather(action)
+    return action
 def control(text):
     global reply
     reply=""
     text=text.replace(" ","")
-    if "天気" in text:
-        action=judge(text)
-        if action:weather(action)
-        else:reply="いつの天気を教えてほしいかわかりませんでした"
-        return yomiage(reply)
-    custom_device_control(text)
-    custom_scene_control(text)
-    switchbot_device_control(text)
-    switchbot_scene_control(text)
+    for i in words:
+        if i in text:
+            judge(text)
+            break
+    else:
+        custom_scene_control(text)
+        switchbot_scene_control(text)
     if reply!="":
         reply="よくわかりませんでした"
-def custom_device_control(text):
-    action=None
+def custom_device_control(text,action):
     for i in custom_devices["deviceList"]:
         if i["deviceName"] in text:
-            if not action:
-                action=judge(text)
             if action:
                 command=i[action].split(" ")
                 if action=="turnOn":reply=f"{i['deviceName']}をオンにします"
@@ -89,17 +95,14 @@ def custom_scene_control(text):
             print("カスタムシーン:",command)
             subprocess.run(command)
             yomiage(reply)
-def switchbot_device_control(text):
-    action=None
-    for i in devices["body"]["infraredRemoteList"]:
-        if i["deviceName"] in text:
-            if not action:
-                action=judge(text)
-            if action=="turnOn":reply=f"{i['deviceName']}をオンにします"
-            else:reply=f"{i['deviceName']}をオフにします"
+def switchbot_device_control(text,action):
+    for i in text:
+        if i in devices_name:
             print(action)
             if action:
-                switchbot.commands(i["deviceName"],action)
+                if action=="turnOn":reply=f"{i}をオンにします"
+                else:reply=f"{i}をオフにします"
+                switchbot.commands(i,action)
             else:
                 reply="なにをするかわかりませんでした"
             yomiage(reply)
@@ -114,7 +117,9 @@ def weather(date):
     api_key=os.getenv("OPENWEATHER_APIKEY")
     location=os.getenv("OPENWEATHER")
     tenki=""
-    if datetime.datetime.now().day==date.day:
+    if not date:
+        tenki="いつの天気を教えてほしいかわかりませんでした"
+    elif datetime.datetime.now().day==date.day:
         weather_json=requests.get(f"https://api.openweathermap.org/data/2.5/weather?{location}&lang=ja&appid={api_key}").json()
         tenki=weather_json["weather"][0]["description"]
     else:
@@ -127,7 +132,7 @@ def weather(date):
                 tenki+=f'１５時の気温は{round((float(weather_json["list"][i+2]["main"]["temp"])-273.15),1)}℃ 天気は{weather_json["list"][i+2]["weather"][0]["description"]}'
     print(tenki,"です")
     reply=f"{tenki}です"
-    return weather_json
+    yomiage(reply)
 def yomiage(text=""):
     global reply
     requests.post("http://192.168.1.2:5000/tts/",json={"message":text,"start":"","end":""})
